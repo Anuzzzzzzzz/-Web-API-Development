0d620fb21576ba083e3d925878381d63e03d059d
danuzz <da.nuzz@danuzzs-MacBook-Air.local>
2024-05-31 22:29:21 +0545

    updated everything

---
+++ src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+    {
+        id : 0,
+        title : "IPhone 11",
+        price : 80000,
+        desc : "IPhone 11",
+        img : "/assets/images/products/iphone11.png"
+    },
+    {
+        id : 1,
+        title : "IPhone 11 Pro Max",
+        price : 10000,
+        desc :"IPhone 11 Pro Max",
+        img : "/assets/images/products/iphone11promax.png"
+    },
+    {
+        id : 2,
+        title : "IPhone 12 Mini",
+        price : 95000,
+        desc :"IPhone 12 Mini",
+        img : "/assets/images/products/iphone12mini.png"
+    },
+    {
+        id : 3,
+        title : "IPhone 12",
+        price : 10500,
+        desc : "IPhone 12",
+        img : "/assets/images/products/iphone12.png"
+    },
+    {
+        id : 4,
+        title : "IPhone 12 Pro",
+        price : 12000,
+        desc :  "IPhone 12 Pro",
+        img : "/assets/images/products/iphone12pro.png"
+    },
+    {
+        id : 5,
+        title : "IPhone 12 Pro Max",
+        price : 16500,
+        desc : "IPhone 12 Pro Max",
+        img : "/assets/images/products/iphone12promax.png"
+    }
+]
+
+export default DATA;
